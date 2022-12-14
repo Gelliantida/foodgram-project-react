de57894199cb0d3cde9b3eaad7099688cc9b59ea
Vovychenko Taras <gelliantida@yandex.com>
2022-12-15 01:24:39 +0800

    v7

--- backend/recipes/management/commands/csv_manager.py
+++ backend/recipes/management/commands/csv_manager.py
@@ -1,23 +1,36 @@
 import csv
 
-from django.conf import settings
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from .models import Ingredient, Tag
+
+file_table = {
+    'ingredients.csv': Ingredient,
+    'tags.csv': Tag,
+}
 
 
 class Command(BaseCommand):
+    help = 'Заполнение БД'
 
     def handle(self, *args, **options):
-        with open(
-            f'{settings.BASE_DIR}/data/ingredients.csv',
-            'r',
-            encoding='utf-8',
-        ) as file:
-            file_reader = csv.reader(file)
-            for row in file_reader:
-                name, measurement_unit = row
-                Ingredient.objects.get_or_create(
-                    name=name,
-                    measurement_unit=measurement_unit)
-        self.stdout.write(self.style.SUCCESS('Ингридиенты загружены!'))
+        for file, table in file_table.items():
+            with open(f'data/{file}', 'r', encoding='utf8') as f:
+                if file == 'ingredients.csv':
+                    fieldnames = ['name', 'measurement_unit']
+                elif file == 'tags.csv':
+                    fieldnames = ['name', 'color', 'slug']
+                dr = csv.DictReader(f, delimiter=',', fieldnames=fieldnames)
+                for row in dr:
+                    if file == 'ingredients.csv':
+                        name = row.pop('name')
+                        unit = row.pop('measurement_unit')
+                        table.objects.get_or_create(name=name,
+                                                    measurement_unit=unit)
+                    elif file == 'tags.csv':
+                        name = row.pop('name')
+                        color = row.pop('color')
+                        slug = row.pop('slug')
+                        table.objects.get_or_create(name=name, color=color,
+                                                    slug=slug)
+                self.stdout.write(f'Таблица {table.__name__} заполнена!')
