03a40a2aeb9f2cfd23816590430cfbf8d6090ed9
Vovychenko Taras <gelliantida@yandex.com>
2022-12-15 01:32:57 +0800

    v7

--- backend/recipes/management/commands/csv_manager.py
+++ backend/recipes/management/commands/csv_manager.py
@@ -2,7 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 
-from .models import Ingredient, Tag
+from recipes.models import Ingredient, Tag
 
 file_table = {
     'ingredients.csv': Ingredient,
