e5d2bce4bee3f477f21b7bd172412396e381e22b
Vovychenko Taras <gelliantida@yandex.com>
2022-12-20 01:32:32 +0800

    v14

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status, filters
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated, SAFE_METHODS
 from rest_framework.response import Response
